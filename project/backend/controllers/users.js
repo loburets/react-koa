f906d80b3106ce9990c2239d83760b04708d9ffb
Dmitriy Loburets <dima-loburec@yandex.ru>
2018-11-04 15:11:02 +0600

    Added few routes for users

--- project/backend/controllers/users.js
+++ project/backend/controllers/users.js
@@ -1,7 +1,25 @@
 exports.getUser = ctx => {
+    let id = ctx.params.id;
+
+    if (typeof id === 'undefined') {
+        // get current user id
+        id = 1;
+    }
+
     const data = {
         name: 'Vasya',
-        message: 'You have got your User!'
+        message: `You have got ${id} User!`
+    };
+
+    return ctx.body = data;
+};
+
+exports.createUser = ctx => {
+
+
+    const data = {
+        name: 'Vasya',
+        message: `You have created new User!`
     };
 
     return ctx.body = data;
--- project/backend/routes.js
+++ project/backend/routes.js
@@ -3,5 +3,7 @@ const usersController = require('./controllers/users');
 const router = new Router();
 
 router.get('/user', usersController.getUser);
+router.get('/users/:id', usersController.getUser);
+router.post('/users', usersController.createUser);
 
 module.exports = router;
